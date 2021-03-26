5564fa5b7b6d0c6774a9b14f16e594d8949df1f8
SushrutThadakamadla42 <77861177+SushrutThadakamadla42@users.noreply.github.com>
2021-03-26 17:17:55 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,35 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2		
+			}
+		
+		this.r=r;
+		
+        this.image = loadImage("paper.png");
+		this.body=Bodies.circle(x, y, r, options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var Pos=this.body.position;		
+
+			push()
+			translate(Pos.x, Pos.y);
+            stroke("black");
+			strokeWeight(4);
+			fill(128,128,128);
+            imageMode(CENTER)
+            image(this.image,0,0,120,140);
+			pop()
+			
+	}
+
+}
